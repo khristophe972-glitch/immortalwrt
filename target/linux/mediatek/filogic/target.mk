084f9e232402679a4c100de4e8f730debfa5ad46
khristophe972-glitch <khristophe972@gmail.com>
2026-07-08 15:03:16 +0200

    mediatek: filogic: add kmod-nft-bridge, kmod-br-netfilter, kmod-nft-netdev for bridge flow offload

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -4,6 +4,7 @@ BOARDNAME:=Filogic 8x0 (MT798x)
 CPU_TYPE:=cortex-a53
 DEFAULT_PACKAGES += \
 	fitblk kmod-crypto-hw-safexcel kmod-nf-conntrack-bridge \
+	kmod-nft-bridge kmod-br-netfilter kmod-nft-netdev \
 	wpad-openssl uboot-envtools
 KERNELNAME:=Image dtbs
 DEFAULT_PROFILE:=openwrt_one
