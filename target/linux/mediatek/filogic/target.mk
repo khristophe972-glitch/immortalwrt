7c27fdb6841eb1f9c616302a143248eec93fb7a6
khristophe972-glitch <khristophe972@gmail.com>
2026-07-10 00:05:37 +0200

    mediatek: filogic: revert kmod-nft-flow-offload, package does not exist

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -5,7 +5,6 @@ CPU_TYPE:=cortex-a53
 DEFAULT_PACKAGES += \
 	fitblk kmod-crypto-hw-safexcel kmod-nf-conntrack-bridge \
 	kmod-nft-bridge kmod-br-netfilter kmod-nft-netdev \
-	kmod-nft-flow-offload \
 	wpad-openssl uboot-envtools
 KERNELNAME:=Image dtbs
 DEFAULT_PROFILE:=openwrt_one
