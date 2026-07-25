92a2d5b1a9bfaa675eb38618d4d2f1a5266ac3d3
khristophe972-glitch <khristophe972@gmail.com>
2026-07-25 20:36:46 +0200

    mediatek: filogic: add kmod-tcp-bbr to default packages (persist BBR across reflashes)

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -5,7 +5,7 @@ CPU_TYPE:=cortex-a53
 DEFAULT_PACKAGES += \
 	fitblk kmod-crypto-hw-safexcel kmod-nf-conntrack-bridge \
 	kmod-nft-bridge kmod-br-netfilter kmod-nft-netdev \
-	ss \
+	ss kmod-tcp-bbr \
 	wpad-openssl uboot-envtools
 KERNELNAME:=Image dtbs
 DEFAULT_PROFILE:=openwrt_one
