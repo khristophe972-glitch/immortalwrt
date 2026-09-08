766b96d1c9ad83c5db86c458b7e474d7fe09f3d6
khristophe972-glitch <khristophe972@gmail.com>
2026-09-09 00:42:43 +0200

    mediatek: filogic: add SCTP userspace tools to GL-MT6000 packages

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1951,7 +1951,7 @@ define Device/glinet_gl-mt6000
   DEVICE_MODEL := GL-MT6000
   DEVICE_DTS := mt7986a-glinet-gl-mt6000
   DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := avahi-dbus-daemon dnsproxy kmod-sched-bpf kmod-usb3 kmod-nft-connlimit kmod-sctp kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware \
+  DEVICE_PACKAGES := avahi-dbus-daemon dnsproxy kmod-sched-bpf kmod-usb3 kmod-nft-connlimit kmod-sctp kmod-sctp-diag conntrack libsctp sctp-tools kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware \
 	automount f2fsck mkf2fs
   IMAGES += factory.bin
   IMAGE/factory.bin := append-kernel | pad-to 32M | append-rootfs
